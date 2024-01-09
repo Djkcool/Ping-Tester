6996a8bd981430ca7ffce7508a5e7906c8412001
Djkcool <>
2024-01-09 11:30:27 +0200

    Added a popular page
    Added a page called popular that has 15 popular websites/services.

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
 # Ping Tester
 A batch script based ping tester for things like Fortnite or any Custom ip.
 
-So far there are 3 options you can choose from Fortnite, Other or Exit.
+So far there are 4 options you can choose from Fortnite, Popular Other or Exit.
+
+In each ping test there is an option to run it 50 times to get a better average.
 ### Fortnite
 In fortnite you can choose a server region and ping test it.
 
-You can also run it 50 times to get a better average.
-
 The reigons it can ping are EU, NAE, NAC, NAW, OCE, ME, ASIA and BRZL
+### Popular
+In the popular page you can choose a popular website or service to ping test.
+
+Currently the servers in the popular page are: Google, Cloudflare, Facebook, Twitter, TikTok, Reddit, Pinterest, Discord, Netflix, Twitch, AWS (Amazon Web Services), Azure, StackOverFlow, OpenAI and Spotify.
 ### Other
 In other you can choose to enter a custom ip and ping test that or enter a custom ip and ping it in better average mode.
 
--- pingtest.bat
+++ pingtest.bat
@@ -4,9 +4,10 @@ title Ping Tester
 cls
 
 echo Djkcools Ping tester :
-echo Type [F] for Fortnite, [O] for Others, [E] to exit
+echo Type [F] for Fortnite, [P] for Popular, [O] for Others, [E] to exit
 
 set f=f
+set p=p
 set o=o
 set e=e
 SET /P rus1=
@@ -14,6 +15,9 @@ SET /P rus1=
 IF %rus1%==%f% goto fortnite
 set f=F
 if %rus1%==%f% goto fortnite
+if %rus1%==%p% goto popular
+set p=P
+if %rus1%==%p% goto popular
 if %rus1%==%o% goto other
 set o=O
 if %rus1%==%o% goto other
@@ -496,6 +500,729 @@ set n=N
 IF %r1%==%n% goto brzl
 
 goto die
+
+:popular
+cls
+title Ping Tester - Popular
+echo You picked Popular
+echo Type [1] for Google, [2] for Cloudflare, [3] for Facebook, [4] for Twitter, [5] for TikTok, [6] for Reddit, [7] for Pinterest, [8] for Discord, [9] for Netflix, [10] for Twitch, [11] for AWS, [12] for Azure, [13] for StackOverFlow, [14] for OpenAI, [15] for Spotify and [B] to go back
+
+set google=1
+set cloudflare=2
+set facebook=3
+set twitter=4
+set tiktok=5
+set reddit=6
+set pinterest=7
+set discord=8
+set netflix=9
+set twitch=10
+set aws=11
+set azure=12
+set stackoverflow=13
+set openai=14
+set spotify=15
+
+set back=b
+SET /P r1=
+
+IF %r1%==%back% goto start
+set back=B
+IF %r1%==%back% goto start
+
+IF %r1%==%google% goto google
+IF %r1%==%cloudflare% goto cloudflare
+IF %r1%==%facebook% goto facebook
+IF %r1%==%twitter% goto twitter
+IF %r1%==%tiktok% goto tiktok
+IF %r1%==%reddit% goto reddit
+IF %r1%==%pinterest% goto pinterest
+IF %r1%==%discord% goto discord
+IF %r1%==%netflix% goto netflix
+IF %r1%==%twitch% goto twitch
+IF %r1%==%aws% goto aws
+IF %r1%==%azure% goto azure
+IF %r1%==%stackoverflow% goto stackoverflow
+IF %r1%==%openai% goto openai
+IF %r1%==%spotify% goto spotify
+
+goto popular
+goto die
+
+:google
+title Ping Tester - Popular - Google
+cls
+echo You picked Popular - Google
+ping 8.8.8.8
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto google
+set r=R
+IF %r1%==%r% goto google
+IF %r1%==%a% goto google50
+set a=A
+IF %r1%==%a% goto google50
+
+goto die
+
+:google50
+cls
+echo Running Better Average Popular - Google
+ping 8.8.8.8 -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto google50
+set r=R
+IF %r1%==%r% goto google50
+IF %r1%==%n% goto google
+set n=N
+IF %r1%==%n% goto google
+
+:cloudflare
+title Ping Tester - Popular - Cloudflare
+cls
+echo You picked Popular - Cloudflare
+ping 1.1.1.1
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto cloudflare
+set r=R
+IF %r1%==%r% goto cloudflare
+IF %r1%==%a% goto cloudflare50
+set a=A
+IF %r1%==%a% goto cloudflare50
+
+goto die
+
+:cloudflare50
+cls
+echo Running Better Average Popular - Cloudflare
+ping 1.1.1.1 -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto cloudflare50
+set r=R
+IF %r1%==%r% goto cloudflare50
+IF %r1%==%n% goto cloudflare
+set n=N
+IF %r1%==%n% goto cloudflare
+
+:facebook
+title Ping Tester - Popular - Facebook
+cls
+echo You picked Popular - Facebook
+ping facebook.com
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto facebook
+set r=R
+IF %r1%==%r% goto facebook
+IF %r1%==%a% goto facebook50
+set a=A
+IF %r1%==%a% goto facebook50
+
+goto die
+
+:facebook50
+cls
+echo Running Better Average Popular - Facebook
+ping facebook.com -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto facebook50
+set r=R
+IF %r1%==%r% goto facebook50
+IF %r1%==%n% goto facebook
+set n=N
+IF %r1%==%n% goto facebook
+
+:twitter
+title Ping Tester - Popular - Twitter
+cls
+echo You picked Popular - Twitter
+ping twitter.com
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto twitter
+set r=R
+IF %r1%==%r% goto twitter
+IF %r1%==%a% goto twitter50
+set a=A
+IF %r1%==%a% goto twitter50
+
+goto die
+
+:twitter50
+cls
+echo Running Better Average Popular - Twitter
+ping twitter.com-n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto twitter50
+set r=R
+IF %r1%==%r% goto twitter50
+IF %r1%==%n% goto twitter
+set n=N
+IF %r1%==%n% goto twitter
+
+:tiktok
+title Ping Tester - Popular - TikTok
+cls
+echo You picked Popular - TikTok
+ping tiktok.com
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto tiktok
+set r=R
+IF %r1%==%r% goto tiktok
+IF %r1%==%a% goto tiktok50
+set a=A
+IF %r1%==%a% goto tiktok50
+
+goto die
+
+:tiktok50
+cls
+echo Running Better Average Popular - TikTok
+ping tiktok.com -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto tiktok50
+set r=R
+IF %r1%==%r% goto tiktok50
+IF %r1%==%n% goto tiktok
+set n=N
+IF %r1%==%n% goto tiktok
+
+:reddit
+title Ping Tester - Popular - Reddit
+cls
+echo You picked Popular - Reddit
+ping reddit.com
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto reddit
+set r=R
+IF %r1%==%r% goto reddit
+IF %r1%==%a% goto reddit50
+set a=A
+IF %r1%==%a% goto reddit50
+
+goto die
+
+:reddit50
+cls
+echo Running Better Average Popular - Reddit
+ping reddit.com -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto reddit50
+set r=R
+IF %r1%==%r% goto reddit50
+IF %r1%==%n% goto reddit
+set n=N
+IF %r1%==%n% goto reddit
+
+:pinterest
+title Ping Tester - Popular - Pinterest
+cls
+echo You picked Popular - Pinterest
+ping pinterest.com
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto pinterest
+set r=R
+IF %r1%==%r% goto pinterest
+IF %r1%==%a% goto pinterest50
+set a=A
+IF %r1%==%a% goto pinterest50
+
+goto die
+
+:pinterest50
+cls
+echo Running Better Average Popular - Pinterest
+ping pinterest.com -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto pinterest50
+set r=R
+IF %r1%==%r% goto pinterest50
+IF %r1%==%n% goto pinterest
+set n=N
+IF %r1%==%n% goto pinterest
+
+:discord
+title Ping Tester - Popular - Discord
+cls
+echo You picked Popular - Discord
+ping discord.com
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto discord
+set r=R
+IF %r1%==%r% goto discord
+IF %r1%==%a% goto discord50
+set a=A
+IF %r1%==%a% goto discord50
+
+goto die
+
+:discord50
+cls
+echo Running Better Average Popular - Discord
+ping discord.com -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto discord50
+set r=R
+IF %r1%==%r% goto discord50
+IF %r1%==%n% goto discord
+set n=N
+IF %r1%==%n% goto discord
+
+:netflix
+title Ping Tester - Popular - Netflix
+cls
+echo You picked Popular - Netflix
+ping netflix.com
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto netflix
+set r=R
+IF %r1%==%r% goto netflix
+IF %r1%==%a% goto netflix50
+set a=A
+IF %r1%==%a% goto netflix50
+
+goto die
+
+:netflix50
+cls
+echo Running Better Average Popular - Netflix
+ping netflix.com -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto netflix50
+set r=R
+IF %r1%==%r% goto netflix50
+IF %r1%==%n% goto netflix
+set n=N
+IF %r1%==%n% goto netflix
+
+:twitch
+title Ping Tester - Popular - Twitch
+cls
+echo You picked Popular - Twitch
+ping live.twitch.tv
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto twitch
+set r=R
+IF %r1%==%r% goto twitch
+IF %r1%==%a% goto twitch50
+set a=A
+IF %r1%==%a% goto twitch50
+
+goto die
+
+:twitch50
+cls
+echo Running Better Average Popular - Twitch
+ping live.twitch.tv -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto twitch50
+set r=R
+IF %r1%==%r% goto twitch50
+IF %r1%==%n% goto twitch
+set n=N
+IF %r1%==%n% goto twitch
+
+:aws
+title Ping Tester - Popular - AWS
+cls
+echo You picked Popular - AWS
+ping aws.amazon.com
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto aws
+set r=R
+IF %r1%==%r% goto aws
+IF %r1%==%a% goto aws50
+set a=A
+IF %r1%==%a% goto aws50
+
+goto die
+
+:aws50
+cls
+echo Running Better Average Popular - AWS
+ping aws.amazon.com -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto aws50
+set r=R
+IF %r1%==%r% goto aws50
+IF %r1%==%n% goto aws
+set n=N
+IF %r1%==%n% goto aws
+
+:azure
+title Ping Tester - Popular - Azure
+cls
+echo You picked Popular - Azure
+ping azure.microsoft.com
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto azure
+set r=R
+IF %r1%==%r% goto azure
+IF %r1%==%a% goto azure50
+set a=A
+IF %r1%==%a% goto azure50
+
+goto die
+
+:azure50
+cls
+echo Running Better Average Popular - Azure
+ping azure.microsoft.com -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto azure50
+set r=R
+IF %r1%==%r% goto azure50
+IF %r1%==%n% goto azure
+set n=N
+IF %r1%==%n% goto azure
+
+:stackoverflow
+title Ping Tester - Popular - StackOverFlow
+cls
+echo You picked Popular - StackOverFlow
+ping stackoverflow.com
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto stackoverflow
+set r=R
+IF %r1%==%r% goto stackoverflow
+IF %r1%==%a% goto stackoverflow50
+set a=A
+IF %r1%==%a% goto stackoverflow50
+
+goto die
+
+:stackoverflow50
+cls
+echo Running Better Average Popular - StackOverFlow
+ping stackoverflow.com -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto stackoverflow50
+set r=R
+IF %r1%==%r% goto stackoverflow50
+IF %r1%==%n% goto stackoverflow
+set n=N
+IF %r1%==%n% goto stackoverflow
+
+:openai
+title Ping Tester - Popular - OpenAI
+cls
+echo You picked Popular - OpenAI
+ping api.openai.com
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto openai
+set r=R
+IF %r1%==%r% goto openai
+IF %r1%==%a% goto openai50
+set a=A
+IF %r1%==%a% goto openai50
+
+goto die
+
+:openai50
+cls
+echo Running Better Average Popular - OpenAI
+ping api.openai.com -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto openai50
+set r=R
+IF %r1%==%r% goto openai50
+IF %r1%==%n% goto openai
+set n=N
+IF %r1%==%n% goto openai
+
+:spotify
+title Ping Tester - Popular - Spotify
+cls
+echo You picked Popular - Spotify
+ping spotify.com
+echo Type [R] to repeat, [A] to run better average mode and [B] to go back
+
+set back=b
+set r=r
+set a=a
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto spotify
+set r=R
+IF %r1%==%r% goto spotify
+IF %r1%==%a% goto spotify50
+set a=A
+IF %r1%==%a% goto spotify50
+
+goto die
+
+:spotify50
+cls
+echo Running Better Average Popular - Spotify
+ping spotify.com -n 50
+echo Type [R] to repeat, [N] to go back to normal mode and [B] to go back to the Popular page
+
+set back=b
+set r=r
+set n=n
+SET /P r1=
+
+IF %r1%==%back% goto popular
+set b=B
+IF %r1%==%back% goto popular
+IF %r1%==%r% goto spotify50
+set r=R
+IF %r1%==%r% goto spotify50
+IF %r1%==%n% goto spotify
+set n=N
+IF %r1%==%n% goto spotify
 
 :die
 title Ping Tester - Exit
